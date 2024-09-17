834b0bd81403b8e9de598267899b7ef360e283a8
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-09-16 22:40:48 -0600

    Correción de importacion de imagenes header.js y agregando componente en app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './componets/header';
+import Footer from './componets/footer';
+import Producto from './componets/Producto';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
--- src/componets/header.js
+++ src/componets/header.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Frame1 from './img/Frame 1.png';
+import Frame2 from './img/Frame 2.png';
+import Frame3 from './img/Frame 3.png';
+import Logo from './img/Logo.PNG';
+
+;
 
 function Header(){
     return(
         <header>
-            <img src="/img/Logo.PNG" width="200px" alt="Logo King Mosa">
+            <img src={Logo} width="200px" alt="Logo King Mosa"/>
             <nav>
                 <ul>
-                    <li><a href="#"><img src="img/Frame 1.png" width="200" alt="Menú"></a></li>
-                    <li><a href="#"><img src="img/Frame 2.png" width="200" alt="Menú"></a></li>
-                    <li><a href="#"><img src="img/Frame 3.png" width="200" alt="Menú"></a></li>
+                    <li><a href="#"><img src={Frame1} width="200" alt="Menú"/></a></li>
+                    <li><a href="#"><img src={Frame2} width="200" alt="Promociones"/></a></li>
+                    <li><a href="#"><img src={Frame3} width="200" alt="Contacto"/></a></li>
                 </ul>
             </nav>
         </header>
