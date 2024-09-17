23437f784f90fe4db187727c7f76514157642848
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-09-16 19:57:53 -0600

    Creación de nuestros componentes de nuestra pagina web, y agregar imagenes

---
+++ src/componets/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <img src="/img/Logo.PNG" width="200px" alt="Logo King Mosa">
+            <nav>
+                <ul>
+                    <li><a href="#"><img src="img/Frame 1.png" width="200" alt="Menú"></a></li>
+                    <li><a href="#"><img src="img/Frame 2.png" width="200" alt="Menú"></a></li>
+                    <li><a href="#"><img src="img/Frame 3.png" width="200" alt="Menú"></a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Header;
