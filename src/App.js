5c8dd144500171c3b07ee56b288965f44c38449a
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-09-16 23:00:59 -0600

    Agrega compontentes footer y productos, declarando la funcion producto

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Producto from './componets/Producto';
 import './App.css';
 
 function App() {
+  const productos = [
+    { id: 1, nombre: "Pizza Micro", precio: 35 },
+    { id: 2, nombre: "Pizza Personal", precio: 99 },
+    { id: 3, nombre: "Pizza Grande", precio: 179 },
+  ];
   return (
     <div className="App">
       <Header/>
+      <main>
+        {productos.map(producto => <Producto key={producto.id} nombre={producto.nombre} precio={producto.precio}/> )}
+      </main>
+      <Footer/>
     </div>
   );
 }
